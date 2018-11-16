c7b8a777484381ddd780b810dfb76aaa4765d12e
filiposk <fpavlovic1995@gmail.com>
2018-11-16 13:05:40 +0100

    filiposk
    Created model interfaces

---
+++ VehicleLot.Model.Common/IVehicleModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VehicleLot.Model.Common
+{
+    public interface IVehicleModel
+    {
+        Guid Id { get; set; }
+        Guid VehicleMakeId { get; set; }
+        string Name { get; set; }
+        string Abrv { get; set; }
+    }
+}
---
+++ VehicleLot.Model.Common/IVehiclemake.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VehicleLot.Model.Common
+{
+    public interface IVehicleMake
+    {
+        Guid Id { get; set; }
+        string Name { get; set; }
+        string Abrv { get; set; }
+
+        ICollection<IVehicleModel> VehicleModels { get; set; }
+
+    }
+
+}
